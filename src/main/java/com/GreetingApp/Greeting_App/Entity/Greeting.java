454cb23af9ecd145ed46b2e65db699f1fa6fc90f
Ritik303 <100149666+Ritik303@users.noreply.github.com>
2025-03-17 21:00:32 +0530

    FindById

--- src/main/java/com/GreetingApp/Greeting_App/Entity/Greeting.java
+++ src/main/java/com/GreetingApp/Greeting_App/Entity/Greeting.java
@@ -1,23 +1,31 @@
 package com.GreetingApp.Greeting_App.Entity;
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class Greeting {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String message;
 
+    // Constructors
     public Greeting() {}
 
     public Greeting(String message) {
         this.message = message;
     }
 
+    // Getters and Setters
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getMessage() {
