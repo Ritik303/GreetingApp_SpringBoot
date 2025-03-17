85719ed4b66fac51a13f4216591d8b5f898cbacb
Ritik303 <100149666+Ritik303@users.noreply.github.com>
2025-03-17 21:21:06 +0530

    User Login

--- src/main/java/com/GreetingApp/Greeting_App/Controller/AuthUserController.java
+++ src/main/java/com/GreetingApp/Greeting_App/Controller/AuthUserController.java
@@ -1,7 +1,10 @@
 package com.GreetingApp.Greeting_App.Controller;
 
 import com.GreetingApp.Greeting_App.DTO.AuthUserDTO;
+import com.GreetingApp.Greeting_App.DTO.LoginRequestDTO;
+import com.GreetingApp.Greeting_App.DTO.LoginResponseDTO;
 import com.GreetingApp.Greeting_App.Service.AuthenticationService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,5 +27,11 @@ public class AuthUserController {
     public ResponseEntity<String> registerUser(@Valid @RequestBody AuthUserDTO authUserDTO) {
         authenticationService.registerUser(authUserDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> loginUser(@RequestBody LoginRequestDTO loginRequestDTO) throws MessagingException {
+        LoginResponseDTO response = authenticationService.loginUser(loginRequestDTO);
+        return ResponseEntity.ok(response);
     }
 }
